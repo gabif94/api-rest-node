760847ec21210dfdda6e8779ebc21c3660c7f1c9
Gabi Fresco <gabi94f@gmail.com>
2021-07-06 16:33:53 -0300

    deploy time

--- dataMockup.js
+++ dataMockup.js
@@ -1,19 +1,19 @@
-let notes = [
-	{
-		id: 1,
-		description: 'Buscar repuesto lavarropas',
-		important: true,
-	},
-	{
-		id: 2,
-		description: 'Organizar busqueda empleo',
-		important: true,
-	},
-	{
-		id: 3,
-		description: 'Trabajar en proyectos para seguir aprendiendo',
-		important: true,
-	},
-];
+const notes = [
+  {
+    id: 1,
+    content: 'Buscar repuesto lavarropas',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Organizar busqueda empleo',
+    important: true
+  },
+  {
+    id: 3,
+    content: 'Trabajar en proyectos para seguir aprendiendo',
+    important: true
+  }
+]
 
-module.exports = notes;
+module.exports = notes
--- index.js
+++ index.js
@@ -1,32 +1,62 @@
-const express = require('express');
-const app = express();
-const port = 3001;
+const express = require('express')
+const cors = require('cors')
 
-app.use(express.json());
+const app = express()
+const port = 3001
 
-let notes = require('./dataMockup');
+app.use(cors())
+app.use(express.json())
+
+let notes = require('./dataMockup')
 
 app.get('/api/notes', (req, res) => {
-	res.json(notes);
-});
+  res.json(notes)
+})
 
 app.get('/api/notes/:id', (req, res) => {
-	const {id} = req.params;
-	const data = notes.find(note => note.id == id);
+  const { id } = req.params
+  const data = notes.find(note => note.id === Number(id))
 
-	if (data) {
-		res.json(data);
-	} else {
-		res.status(404).end();
-	}
-});
+  if (data) {
+    res.json(data)
+  } else {
+    res.status(404).end()
+  }
+})
 
 app.delete('/api/notes/:id', (req, res) => {
-	const {id} = req.params;
-	notes = notes.filter(note => note.id !== Number(id));
-	res.status(204).end();
-});
+  const { id } = req.params
+  notes = notes.filter(note => note.id !== Number(id))
+  res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const note = req.body
+
+  if (!note || !note.content) {
+    return res.status(400).json({
+      error: 'content is missing'
+    })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+
+  notes = [...notes, newNote]
+
+  res.status(201).json(newNote)
+})
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' })
+})
 
 app.listen(port, () => {
-	console.log(`App corriendo en puerto ${port}`);
-});
+  console.log(`App corriendo en puerto ${port}`)
+})
